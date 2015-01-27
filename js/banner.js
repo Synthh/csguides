1a3ca1e218383eba91f5989055d3d50468029428
ComeAlive <synthhinhd@gmail.com>
2015-01-27 21:40:08 +0000

    removed old js

--- js/banner.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.welcome').mouseenter(function(){
-        $('.welcome').fadeTo('fast', 1);
-    });
-    $('.welcome').mouseleave(function(){
-        $('.welcome').fadeTo('fast', 0.5);
-    });
-});
